4a381e065c5f1a6c30688f557791feec466cc223
Vethya <synasopheakvethya@gmail.com>
2025-04-17 10:44:53 +0700

    allow adding custom AI name

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,11 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const AI_NAME = process.env.AI_NAME;
+
 export const systemPrompt = `
-You are Vethya AI, a helpful learning assistant designed to assist users. Your response should be concise, refrain from giving long answers until necessary.
+You are ${AI_NAME}, a helpful learning assistant designed to assist users. Your response should be concise, refrain from giving long answers until necessary.
 
 Under "Context:" you will find the context of the conversation. You shouldn't repeat the content in the context in your response as it's specially formatted for you to see only. You should only use the context to understand the conversation better and provide a more relevant answer.
 `;
