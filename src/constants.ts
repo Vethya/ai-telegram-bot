4e788342eede80c386c8296796485e7d9649cecb
Vethya <synasopheakvethya@gmail.com>
2025-04-17 10:46:28 +0700

    remove unnecessary prompt in the system prompt

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,4 @@ const AI_NAME = process.env.AI_NAME;
 
 export const systemPrompt = `
 You are ${AI_NAME}, a helpful learning assistant designed to assist users. Your response should be concise, refrain from giving long answers until necessary.
-
-Under "Context:" you will find the context of the conversation. You shouldn't repeat the content in the context in your response as it's specially formatted for you to see only. You should only use the context to understand the conversation better and provide a more relevant answer.
 `;
